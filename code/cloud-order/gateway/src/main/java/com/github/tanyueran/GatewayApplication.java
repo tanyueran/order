cbeaf2d8e14676e68b13f74d89d8792a153d02ad
unknown <txloveu@163.com>
2020-09-01 13:52:50 +0800

    no commit message

--- code/cloud-order/gateway/src/main/java/com/github/tanyueran/GatewayApplication.java
+++ code/cloud-order/gateway/src/main/java/com/github/tanyueran/GatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class CommonApplication {
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CommonApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
